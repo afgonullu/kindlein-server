95bdfd40c6e8af6a8923e130986f915dc59f29a3
A Faruk Gonullu <afgonullu@gmail.com>
2021-01-23 16:50:56 +0300

    build: connect to MongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
 import { ApolloServer } from "apollo-server-express";
 import { schema } from "./graphql/schema";
+
+import { MONGODB_URI } from "./utils/config";
+
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error: { message: string }) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
 
 const app = express();
 const server = new ApolloServer({
---
+++ src/utils/config.ts
@@ -0,0 +1,5 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const MONGODB_URI = <string>process.env.MONGODB_URI;
